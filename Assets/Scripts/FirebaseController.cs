e2ee592022f988d47436719c50c856ae9800a6c8
Alessia-1998 <alessiacaristo98@gmail.com>
2024-09-01 16:02:36 +0200

    Aggiunti Scena e Script per Firebase
    A causa di alcuni problemi ho preferito (per il momento) mantenere disattivata la schermata di autenticazione dell'utente durante l'esecuzione del gioco.
    
    NOTA: i file "FirebaseCppAPP-12_2_0.so" e "FirebaseCppAPP-12_2_0.bundle" nella cartella "Asset\Plugin\x86_64" sono stati esclusi da questo Commit perchè superano i limiti di grandezza imposti da GitHub

---
+++ Assets/Scripts/FirebaseController.cs
@@ -0,0 +1,347 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using UnityEditor.VersionControl;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase;
+using Firebase.Auth;
+using System;
+using System.Threading.Tasks;
+using Firebase.Extensions;
+using UnityEngine.SceneManagement;
+
+public class FirebaseController : MonoBehaviour
+{
+    public GameObject loginPanel, signupPanel, profilePanel, forgetPasswordPanel, notificationPanel;
+
+    public InputField loginEmail, loginPassword, signupEmail, signupPassword, signupCPassword, signupUserName, forgetPassEmail;
+
+    public Text notif_Title_Text, notif_Message_Text, profileUserName_Text, profileUserEmail_Text;
+
+    public Toggle rememberMe;
+
+    Firebase.Auth.FirebaseAuth auth;
+    Firebase.Auth.FirebaseUser user;
+
+    bool isSignIn = false;
+
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                InitializeFirebase();
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+
+    public void OpenLoginPanel()
+    {
+        loginPanel.SetActive(true);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        forgetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenSignUpPanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(true);
+        profilePanel.SetActive(false);
+        forgetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenProfilePanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(true);
+        forgetPasswordPanel.SetActive(false);
+    }
+
+    public void OpenForgetPassPanel()
+    {
+        loginPanel.SetActive(false);
+        signupPanel.SetActive(false);
+        profilePanel.SetActive(false);
+        forgetPasswordPanel.SetActive(true);
+    }
+
+    public void LoginUser()
+    {
+        if(string.IsNullOrEmpty(loginEmail.text)&&string.IsNullOrEmpty(loginPassword.text))
+        {
+            showNotificationMessage("Error", "Fields Empty! Please Input Details In All Fields");
+            return;
+        }
+
+        // Do Login
+        SignInUser(loginEmail.text, loginPassword.text);
+        SceneManager.LoadScene(sceneName: "MainScene");
+    }
+
+    public void SignUpUser()
+    {
+        if(string.IsNullOrEmpty(signupEmail.text) && string.IsNullOrEmpty(signupPassword.text) && string.IsNullOrEmpty(signupCPassword.text) && string.IsNullOrEmpty(signupUserName.text))
+        {
+            showNotificationMessage("Error", "Fields Empty! Please Input Details In All Fields");
+            return;
+        }
+
+        // Do DoSignUp
+        CreateUser(signupEmail.text, signupPassword.text, signupUserName.text);
+    }
+
+    public void forgetPass()
+    {
+        if (string.IsNullOrEmpty(forgetPassEmail.text))
+        {
+            showNotificationMessage("Error", "Fields Empty! Please Input Details In All Fields");
+            return;
+        }
+        forgetPasswordSubmit(forgetPassEmail.text);
+    }
+
+    private void showNotificationMessage(string title,string message)
+    {
+        notif_Title_Text.text = "" + title;
+        notif_Message_Text.text = "" + message;
+
+        notificationPanel.SetActive(true);
+    }
+
+    public void CloseNotif_Panel()
+    {
+        notif_Title_Text.text = "" ;
+        notif_Message_Text.text = "" ;
+
+        notificationPanel.SetActive(false);
+    }
+
+    public void LogOut()
+    {
+        auth.SignOut();
+        profileUserEmail_Text.text = "";
+        profileUserName_Text.text = "";
+        OpenLoginPanel();
+    }
+
+    void CreateUser(string email, string password, string Username)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                Exception exception = task.Exception;
+                Firebase.FirebaseException firebaseEx = exception as Firebase.FirebaseException;
+                if (firebaseEx != null)
+                {
+                    var errorCode = (AuthError)firebaseEx.ErrorCode;
+                    showNotificationMessage("Error", GetErrorMessage(errorCode));
+                }
+
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+
+            UpdateUserProfile(Username);
+        });
+    }
+
+    public void SignInUser(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                
+                foreach(Exception exception in task.Exception.Flatten().InnerExceptions)
+                {
+                    Firebase.FirebaseException firebaseEx = exception as Firebase.FirebaseException;
+                    if (firebaseEx != null)
+                    {
+                        var errorCode = (AuthError)firebaseEx.ErrorCode;
+                        showNotificationMessage("Error", GetErrorMessage(errorCode));
+                    }
+                }
+                
+
+                return;
+            }
+
+            Firebase.Auth.AuthResult newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.User.DisplayName, newUser.User.UserId);
+            profileUserEmail_Text.text = "" + newUser.User.DisplayName;
+            profileUserEmail_Text.text = "" + newUser.User.Email;
+
+            OpenLoginPanel();
+        });
+    }
+
+    void InitializeFirebase()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.StateChanged += AuthStateChanged;
+        AuthStateChanged(this, null);
+    }
+
+    void AuthStateChanged(object sender, System.EventArgs eventArgs)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signedIn = user != auth.CurrentUser && auth.CurrentUser != null
+                && auth.CurrentUser.IsValid();
+            if (!signedIn && user != null)
+            {
+                Debug.Log("Signed out " + user.UserId);
+            }
+            user = auth.CurrentUser;
+            if (signedIn)
+            {
+                Debug.Log("Signed in " + user.UserId);
+                isSignIn = true;
+            }
+        }
+    }
+
+    // Handle removing subscription and reference to the Auth instance.
+    // Automatically called by a Monobehaviour after Destroy is called on it.
+    void OnDestroy()
+    {
+        auth.StateChanged -= AuthStateChanged;
+        auth = null;
+    }
+
+    void UpdateUserProfile(string UserName)
+    {
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            Firebase.Auth.UserProfile profile = new Firebase.Auth.UserProfile
+            {
+                DisplayName = UserName,
+                PhotoUrl = new System.Uri("https://dummyimage.com/150"),
+            };
+            user.UpdateUserProfileAsync(profile).ContinueWith(task => {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("UpdateUserProfileAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("UpdateUserProfileAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                Debug.Log("User profile updated successfully.");
+
+                showNotificationMessage("Alert", "Account Successfully Created" );
+            });
+        }
+    }
+
+    bool isSigned = false;
+
+    void Update()
+    {
+        if(isSignIn)
+        {
+            if(isSigned)
+            {
+                isSigned = true;
+                profileUserEmail_Text.text = "" + user.DisplayName;
+                profileUserEmail_Text.text = "" + user.Email;
+
+                OpenLoginPanel();
+            }
+        }
+    }
+
+    private static string GetErrorMessage(AuthError errorCode)
+    {
+        var message = "";
+        switch (errorCode)
+        {
+            case AuthError.AccountExistsWithDifferentCredentials:
+                message = "Account Not Exist";
+                break;
+            case AuthError.MissingPassword:
+                message = "Missing Password";
+                break;
+            case AuthError.WeakPassword:
+                message = "Password So Weak";
+                break;
+            case AuthError.WrongPassword:
+                message = "Wrong Password";
+                break;
+            case AuthError.EmailAlreadyInUse:
+                message = "Your Email Already in Use";
+                break;
+            case AuthError.InvalidEmail:
+                message = "Your Email Invalid";
+                break;
+            case AuthError.MissingEmail:
+                message = "Your Email Missing";
+                break;
+            default:
+                message = "Invalid Error";
+                break;
+        }
+        return message;
+    }
+
+    void forgetPasswordSubmit(string forgetPasswordEmail)
+    {
+        auth.SendPasswordResetEmailAsync(forgetPasswordEmail).ContinueWithOnMainThread(task=>{
+
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SendPasswordResetEmailAsync was canceled");
+            }
+
+            if(task.IsFaulted)
+            {
+                foreach (Exception exception in task.Exception.Flatten().InnerExceptions)
+                {
+                    Firebase.FirebaseException firebaseEx = exception as Firebase.FirebaseException;
+                    if (firebaseEx != null)
+                    {
+                        var errorCode = (AuthError)firebaseEx.ErrorCode;
+                        showNotificationMessage("Error", GetErrorMessage(errorCode));
+                    }
+                }
+            }
+            showNotificationMessage("Alert", "Successfully Send Email For Reset Password");
+
+        });
+    }
+}
